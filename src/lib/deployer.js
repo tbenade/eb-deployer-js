fd357d9d7cc5c34f4813b7b12c5269d03d428d2d
Behrang Saeedzadeh <behrangsa@gmail.com>
2015-09-05 09:56:53 +1000

    Actually, console all the things!

--- src/lib/deployer.js
+++ src/lib/deployer.js
@@ -1,13 +1,13 @@
 var AWS = require('aws-sdk'),
-    FSM = require('./statemachine'),
-    path = require('path'),
-    l = require('./logger');
+  FSM = require('./statemachine'),
+  path = require('path'),
+  l = require('./logger');
 
-module.exports.deploy = function (config, options) {
-    var services = configureServices(config),
-        stateMachine = configureStateMachine(config, services, options);
+module.exports.deploy = function(config, options) {
+  var services = configureServices(config),
+    stateMachine = configureStateMachine(config, services, options);
 
-    stateMachine.run({});
+  stateMachine.run({});
 };
 
 /**
@@ -17,27 +17,27 @@ module.exports.deploy = function (config, options) {
  * @param {object} config
  */
 function configureAWS(config) {
-    AWS.config.update({
-        region: config.Region,
-        maxRetries: 10,
-        logger: console.log
-    });
+  AWS.config.update({
+    region: config.Region,
+    maxRetries: 10,
+    logger: console
+  });
 
-    AWS.events = new AWS.SequentialExecutor();
+  AWS.events = new AWS.SequentialExecutor();
 
-    AWS.events.on('send', function (resp) {
-        resp.startTime = new Date().getTime();
-    }).on('complete', function (resp) {
-        resp.endTime = new Date().getTime();
+  AWS.events.on('send', function(resp) {
+    resp.startTime = new Date().getTime();
+  }).on('complete', function(resp) {
+    resp.endTime = new Date().getTime();
 
-        if (resp.error) {
-            l.error("Error calling %s on %s : %j", resp.request.operation, resp.request.service.endpoint.host, resp.error);
-        } else {
-            l.debug("%s took %d seconds %j", resp.request.operation, ((resp.endTime - resp.startTime) / 1000), resp.request.service);
-        }
-    });
+    if (resp.error) {
+      l.error("Error calling %s on %s : %j", resp.request.operation, resp.request.service.endpoint.host, resp.error);
+    } else {
+      l.debug("%s took %d seconds %j", resp.request.operation, ((resp.endTime - resp.startTime) / 1000), resp.request.service);
+    }
+  });
 
-    return AWS;
+  return AWS;
 }
 
 /**
@@ -46,9 +46,9 @@ function configureAWS(config) {
  * @return {object}
  */
 function configureServices(config) {
-    return {
-        AWS: configureAWS(config)
-    };
+  return {
+    AWS: configureAWS(config)
+  };
 }
 
 /**
@@ -65,37 +65,38 @@ function configureServices(config) {
  * @return {object} A configured state machine
  */
 function configureStateMachine(config, services, options) {
-    var states = loadStrategy(options.strategy), stateHandlers = {};
+  var states = loadStrategy(options.strategy),
+    stateHandlers = {};
 
-    function stateMachineTransitionHandler(e) {
-        return function (fsm, state, data) {
+  function stateMachineTransitionHandler(e) {
+    return function(fsm, state, data) {
 
-            l.debug("State machine event: %s\t State: %s", e, state.name);
+      l.debug("State machine event: %s\t State: %s", e, state.name);
 
-            if (!stateHandlers[state.name]) {
-                var handler = loadStrategyState(options.strategy, state.name);
-                stateHandlers[state.name] = handler(config, services, options);
-            }
+      if (!stateHandlers[state.name]) {
+        var handler = loadStrategyState(options.strategy, state.name);
+        stateHandlers[state.name] = handler(config, services, options);
+      }
 
-            if (typeof stateHandlers[state.name][e] === "function") {
-                l.banner("[%s] %s", e, state.name);
-                l.trace("Data = %j", data);
+      if (typeof stateHandlers[state.name][e] === "function") {
+        l.banner("[%s] %s", e, state.name);
+        l.trace("Data = %j", data);
 
-                stateHandlers[state.name][e].apply(stateHandlers[state.name], [fsm, data]);
-            } else {
-                l.warn("State %s has no handler for the '%s' event.", state.name, e);
-            }
-        };
-    }
+        stateHandlers[state.name][e].apply(stateHandlers[state.name], [fsm, data]);
+      } else {
+        l.warn("State %s has no handler for the '%s' event.", state.name, e);
+      }
+    };
+  }
 
-    return new FSM(states).bind(FSM.EXIT, stateMachineTransitionHandler("exit")).bind(FSM.ENTER, stateMachineTransitionHandler("enter")).bind(FSM.CHANGE, stateMachineTransitionHandler("activate"));
+  return new FSM(states).bind(FSM.EXIT, stateMachineTransitionHandler("exit")).bind(FSM.ENTER, stateMachineTransitionHandler("enter")).bind(
+    FSM.CHANGE, stateMachineTransitionHandler("activate"));
 }
 
 function loadStrategy(strategy) {
-    return require('../strategies/' + strategy + '/config.js');
+  return require('../strategies/' + strategy + '/config.js');
 }
 
 function loadStrategyState(strategy, state) {
-    return require('../strategies/' + strategy + '/states/' + state);
+  return require('../strategies/' + strategy + '/states/' + state);
 }
-
--- src/strategies/blue-green/states/terminating-environment.js
+++ src/strategies/blue-green/states/terminating-environment.js
@@ -1,33 +1,33 @@
 var Q = require('q'),
-    _ = require('lodash'),
-    l = require('../../../lib/logger.js'),
-    helpers = require('../../../lib/helpers');
+  _ = require('lodash'),
+  l = require('../../../lib/logger.js'),
+  helpers = require('../../../lib/helpers');
 
-module.exports = function (config, services, args) {
+module.exports = function(config, services, args) {
 
-    var eb = new services.AWS.ElasticBeanstalk({
-        maxRetries: 10,
-        logger: console.log
+  var eb = new services.AWS.ElasticBeanstalk({
+    maxRetries: 10,
+    logger: console
+  });
+
+  function terminateEnvironment(applicationName, environmentName) {
+    return Q.ninvoke(eb, "terminateEnvironment", {
+      EnvironmentName: environmentName,
+      TerminateResources: true
+    }).then(function(result) {
+      return helpers.waitForEnvironment(eb, applicationName, environmentName, function(env) {
+        return env.Status == 'Terminated';
+      });
     });
+  }
 
-    function terminateEnvironment(applicationName, environmentName) {
-        return Q.ninvoke(eb, "terminateEnvironment", {
-            EnvironmentName:    environmentName,
-            TerminateResources: true
-        }).then(function (result) {
-            return helpers.waitForEnvironment(eb, applicationName, environmentName, function (env) {
-                return env.Status == 'Terminated';
-            });
-        });
+  return {
+    activate: function(fsm, data) {
+      terminateEnvironment(config.ApplicationName, data.targetEnvironment.name).then(function(env) {
+        l.success("Environment %s terminated.", data.targetEnvironment.name);
+        fsm.doAction("next", data);
+      }).fail(helpers.genericRollback(fsm, data));
+
     }
-
-    return {
-        activate: function (fsm, data) {
-            terminateEnvironment(config.ApplicationName, data.targetEnvironment.name).then(function (env) {
-                l.success("Environment %s terminated.", data.targetEnvironment.name);
-                fsm.doAction("next", data);
-            }).fail(helpers.genericRollback(fsm, data));
-
-        }
-    }
+  }
 };
