98f33184c2b07f28d2a2734ba35e19d107a9dca5
Terrence Benade <tbenade@seek.com.au>
2015-09-23 08:55:15 +1000

    inplace start

--- src/strategies/in-place/config.js
+++ src/strategies/in-place/config.js
@@ -1,44 +1,45 @@
 module.exports = {
-    initial: "merging-configuration",
+  initial: "merging-configuration",
 
-    states: {
+  states: {
 
-        "merging-configuration":{
-            transitions: {
-                next:     "preparing-bucket",
-                rollback: "rolling-back"
-            }
-        },
-        "preparing-bucket":{
-            transitions: {
-                next:     "uploading-version",
-                rollback: "rolling-back"
-            }
-        },
-        "uploading-version":{
-            transitions: {
-                next:     "deploying-resources",
-                rollback: "rolling-back"
-            }
-        },
-        "deploying-resources":{
-            transitions: {
-                next:     "deploying-version",
-                rollback: "rolling-back"
-            }
-        },
-        "deploying-version":{
-            transitions: {
-                next: "running-tests"
-            }
-        },
-        "running-tests":{
-            transitions: {
-                next:     "completed",
-                rollback: "rolling-back"
-            }
-        },
-        "rolling-back":{},
-        "completed":{}
-    }
+    "merging-configuration": {
+      transitions: {
+        next: "preparing-bucket",
+        rollback: "rolling-back"
+      }
+    },
+    "preparing-bucket": {
+      transitions: {
+        next: "uploading-version",
+        rollback: "rolling-back"
+      }
+    },
+    "uploading-version": {
+      transitions: {
+        next: "deploying-resources",
+        rollback: "rolling-back"
+      }
+    },
+    "deploying-resources": {
+      transitions: {
+        next: "deploying-version",
+        rollback: "rolling-back"
+      }
+    },
+    "deploying-version": {
+      transitions: {
+        next: "running-tests",
+        rollback: "rolling-back"
+      }
+    },
+    "running-tests": {
+      transitions: {
+        next: "completed",
+        rollback: "rolling-back"
+      }
+    },
+    "rolling-back": {},
+    "completed": {}
+  }
 };
